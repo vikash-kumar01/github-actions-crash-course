01d4646823e529045d318baa4540ee8fbf81d51e
vikash <vikashashoke@gmail.com>
2024-12-24 10:44:17 +0530

    Changes the Labels message

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
-          Mr. DevOps
-        </p>
+        <p>Github Actions Fastrack Tutorial - By Mr. DevOps</p>
         <a
           className="App-link"
           href="https://youtube.com/@MrDevOps"
