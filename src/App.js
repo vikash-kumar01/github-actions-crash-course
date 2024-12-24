70e26607d1ae5ea96d318fce0fbb45a49db4a76b
vikash <vikashashoke@gmail.com>
2024-12-24 10:12:27 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://youtube.com/@DevOpsPro"
+          href="https://youtube.com/@MrDevOps"
           target="_blank"
           rel="noopener noreferrer"
         >
